106bc1ad97337ccea68c29bb0e0a53676811ad0d
Rodrigo Corsi <rodrigocorsi@gmail.com>
2022-11-30 08:41:45 -0300

    define page language

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+import React from "react";
+
+const Document: React.FC = () => {
+  return (
+    <Html lang="pt">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
