a89f025a488280097e327d585c37a46a6baf37bf
Rodrigo Corsi <rodrigocorsi@gmail.com>
2022-12-13 07:31:13 -0300

    fix next.config accept another subdomains for api-footbal

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["media.api-sports.io"],
+    domains: [
+      "media.api-sports.io",
+      "media-1.api-sports.io",
+      "media-2.api-sports.io",
+      "media-3.api-sports.io",
+      "media-4.api-sports.io",
+    ],
   },
 };
 
