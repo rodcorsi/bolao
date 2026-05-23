22dbd7c977d9eee5b3bc61447e46c5de2aa8987f
Rodrigo Corsi <rodrigocorsi@gmail.com>
2026-05-23 14:31:01 -0300

    disable user scale

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
 import "../styles/globals.css";
 
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
